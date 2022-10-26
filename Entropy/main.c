851cf2c432b844ebe9c17339953659519f2da631
DEMIDOB <5OxymA16TytlikuGIT>
2022-10-26 20:06:53 +0200

    Initial Commit

---
+++ Entropy/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Entropy
+//
+//  Created by Даня Демидов on 26.10.22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
